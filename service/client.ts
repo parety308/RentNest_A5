afb8523a3c71e250de66b6ad3d4a9ac42a0dd8aa
MD Parvez Hasan <md.parvez.hasan.dev@gmail.com>
2026-08-03 23:16:00 +0600

    added after apiclient

--- service/client.ts
+++ service/client.ts
@@ -1,19 +1,14 @@
-import { cookies } from "next/headers";
-
 const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export async function apiClient(
   endpoint: string,
   options?: RequestInit
 ) {
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-
   const response = await fetch(`${API_URL}${endpoint}`, {
     ...options,
+    credentials: "include",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
       ...options?.headers,
     },
   });
