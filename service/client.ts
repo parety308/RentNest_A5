91b81b804d9dbbd487c8d307f03fc5bd09fff34d
MD Parvez Hasan <md.parvez.hasan.dev@gmail.com>
2026-08-05 03:40:02 +0600

    added fixed version

--- service/client.ts
+++ service/client.ts
@@ -1,11 +1,10 @@
-const SERVER_BASE_URL =
-  process.env.NEXT_PUBLIC_BACKEND_URL;
+const SERVER_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-export async function apiClient(
-  endpoint: string,
-  options?: RequestInit
-) {
-  const response = await fetch(`${SERVER_BASE_URL}${endpoint}`, {
+export async function apiClient(endpoint: string, options?: RequestInit) {
+  const isBrowser = typeof window !== "undefined";
+  const baseUrl = isBrowser ? "/api" : SERVER_BASE_URL;
+
+  const response = await fetch(`${baseUrl}${endpoint}`, {
     ...options,
     credentials: "include",
     headers: {
@@ -16,10 +15,7 @@ export async function apiClient(
 
   if (!response.ok) {
     const errorText = await response.text();
-
-    throw new Error(
-      `Status: ${response.status}\n${errorText}`
-    );
+    throw new Error(`Status: ${response.status}\n${errorText}`);
   }
 
   return response.json();
